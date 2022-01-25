0663588ea7007cb401697b014ed21b0f74b5cd67
chuljin Hwang <chljin.hwang@gmail.com>
2022-01-25 19:43:09 +0900

    Led board test (state and binding)

--- LEDBoard/LEDBoard/ContentView.swift
+++ LEDBoard/LEDBoard/ContentView.swift
@@ -27,6 +27,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(textContent: <#T##Binding<String>#>)
+        ContentView(textContent: Text("ddd"))
     }
 }
